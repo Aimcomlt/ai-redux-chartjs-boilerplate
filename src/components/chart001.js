80990c02bc5a05d1e1fe0e8dbda28a563b878671
Aime Comeault <robinhoodblock71@gmail.com>
2025-08-20 16:08:52 -0600

    Fix label text and tension options

--- src/components/chart001.js
+++ src/components/chart001.js
@@ -10,7 +10,7 @@ import {
   Legend,
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import { getData } from '../features/normalizerFactories/btc-usdt';
 
 ChartJS.register(
@@ -20,7 +20,7 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
 );
 
 export const options = {
@@ -36,52 +36,148 @@ export const options = {
   },
 };
 
-
 export let chartSize = [];
 //////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
 
 export function ChartI() {
-    const dispatch = useDispatch();
-    const state = useSelector(state => state.brain);
-    const [tickAmount, setTickAmount] = useState(5);
-    chartSize.push(tickAmount);
-if(chartSize.length >= 2) {chartSize.splice(0, 1)}
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.brain);
+  const [tickAmount, setTickAmount] = useState(5);
+  chartSize.push(tickAmount);
+  if (chartSize.length >= 2) {
+    chartSize.splice(0, 1);
+  }
 
-    const fetchData = (time) => {
-    dispatch(getData({
-       time: time,
-       tickAmount: tickAmount
-    }))
+  const fetchData = (time) => {
+    dispatch(
+      getData({
+        time: time,
+        tickAmount: tickAmount,
+      }),
+    );
     dispatch({
-        type: "SUCCESS_BITCOIN",
-        payload: {},
-        tickAmount
-    })
-}
-    return <div style={{display: 'flexbox', width: '900px', marginTop: '30px'}}>
+      type: 'SUCCESS_BITCOIN',
+      payload: {},
+      tickAmount,
+    });
+  };
+  return (
+    <div style={{ display: 'flexbox', width: '900px', marginTop: '30px' }}>
+      <div
+        style={{
+          width: '900px',
+          backgroundColor: 'rgba(10,12,40,0.9)',
+          borderRadius: '9px',
+        }}
+      >
+        <Line data={state.data} options={{ responsive: true }} />
+        <br />
+        <button
+          kind="primary"
+          size="2x2"
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            marginBottom: '12px',
+          }}
+          onClick={() =>
+            setInterval(() => {
+              fetchData('min1');
+            }, 60100)
+          }
+        >
+          ⏳START 1 MIN BRAIN CYCLE
+        </button>
+        <button
+          kind="primary"
+          size="2x2"
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            marginBottom: '12px',
+          }}
+          onClick={() => fetchData('min1')}
+        >
+          START TRAINNING SESSIONS
+        </button>
+        <br />
+      </div>
 
+      <p>{tickAmount}</p>
+      <br />
 
-<div style={{width: '900px', backgroundColor: 'rgba(10,12,40,0.9)', borderRadius: '9px'}}>
-<Line  data={state.data} options={{responsive: true}}/> 
-              <br />
-              <button kind='primary' size='2x2' style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', marginBottom: '12px'}} onClick={() => setInterval(() => {fetchData("min1")}, 60100)}>⏳START 1 MIN BRAIN CYCLE</button>
-              <button kind='primary' size='2x2' style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', marginBottom: '12px'}} onClick={() => fetchData("min1")}>START TRAINNING SESSIONS</button>
-              <br /> 
-</div>
+      <div style={{ display: 'flexbox', flexDirection: 'row' }}>
+        <button
+          onClick={() => setTickAmount(5)}
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            width: '60px',
+          }}
+        >
+          5
+        </button>
+        <button
+          onClick={() => setTickAmount(10)}
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            width: '60px',
+          }}
+        >
+          10
+        </button>
+        <button
+          onClick={() => setTickAmount(15)}
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            width: '60px',
+          }}
+        >
+          15
+        </button>
+        <button
+          onClick={() => setTickAmount(20)}
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            width: '60px',
+          }}
+        >
+          20
+        </button>
+        <button
+          onClick={() => setTickAmount(30)}
+          style={{
+            borderRadius: '9px',
+            marginTop: '25px',
+            marginRight: '12px',
+            width: '60px',
+          }}
+        >
+          30
+        </button>
+      </div>
 
-              <p>{tickAmount}</p>
-              <br />
-
-          <div style={{display: 'flexbox', flexDirection: 'row'}}>
-              <button onClick={() => setTickAmount(5)} style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', width: '60px'}}>5</button>
-              <button onClick={() => setTickAmount(10)} style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', width: '60px'}}>10</button>
-              <button onClick={() => setTickAmount(15)} style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', width: '60px'}}>15</button>
-              <button onClick={() => setTickAmount(20)} style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', width: '60px'}}>20</button>
-              <button onClick={() => setTickAmount(30)} style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', width: '60px'}}>30</button>
-            </div>
-
-                 <button onClick={() => setTickAmount(500)} style={{ borderRadius: '9px', marginTop: '25px', marginRight: '12px', width: '60px'}}>ALL TIME</button>
-    </div>;
-    
+      <button
+        onClick={() => setTickAmount(500)}
+        style={{
+          borderRadius: '9px',
+          marginTop: '25px',
+          marginRight: '12px',
+          width: '60px',
+        }}
+      >
+        ALL TIME
+      </button>
+    </div>
+  );
 }
